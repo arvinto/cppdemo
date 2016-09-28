91692cea79686150c059fb5844ef7b7c5bf58b75
John Arvin Aboque <arvinaboque@arvinaboque.local>
2016-09-28 14:15:54 +0800

    Failed commit test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 
 int main() {
     std::cout << "Hello, World! " << add(3,4 )<< std::endl;
-    std::cout << "This is to test jenkins" << std::endl;
+    std::cout << "This is to test jenkins" << std::endl
     return 0;
 }
